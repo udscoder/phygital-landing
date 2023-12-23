ba767965615972019f5f08cdbc97ba0e792b9740
udscoder <udscoder@gmail.com>
2023-12-23 13:37:20 +0500

    added redirect function

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { Introduction } from "./componenets/introduction/Introduction";
 import { Project } from "./componenets/project/Project";
 import { Games } from "./componenets/games/Games";
@@ -9,25 +9,25 @@ import { GamesMobile } from "./componenets/games/GamesMobile";
 import { AndroidsMobile } from "./componenets/androids/AndroidsMobile";
 import { IntroductionMobile } from "./componenets/introduction/IntroductionMobile";
 
-function redirectToApp() {
-    const searchParams = new URLSearchParams(window.location.search);
-
-    const bookingId = searchParams.get('bookingId')
-    const ownerId = searchParams.get('ownerId')
-
-    const appUrl ='phygital://share?bookingId=${bookingId}&ownerId=${ownerId}'
-
-    if (navigator.userAgent.match(/Android/i))
-        document.location = appUrl;
-    else
-        window.location.replace(appUrl);
-
-}
-
-redirectToApp()
-
 function App() {
     const windowWidth: any = useRef(window.innerWidth);
+
+    useEffect(() => {
+        redirectToApp();
+    }, []);
+
+    function redirectToApp() {
+        const searchParams = new URLSearchParams(window.location.search);
+        const bookingId = searchParams.get('bookingId');
+        const ownerId = searchParams.get('ownerId');
+        const appUrl = `phygital://share?bookingId=${bookingId}&ownerId=${ownerId}`;
+
+        if (navigator.userAgent.match(/Android/i)) {
+            document.location = appUrl;
+        } else {
+            window.location.replace(appUrl);
+        }
+    }
 
     return (
         <div className="App">
